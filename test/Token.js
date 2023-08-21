2d5911df6d359488141cc1699c3f48a81ceae467
Ravi Tokas <ravitokas90@gmail.com>
2023-08-21 19:44:43 +0530

    replaced variables in test

--- test/Token.js
+++ test/Token.js
@@ -14,20 +14,25 @@ describe("Token", () => {
   });
 
   describe("Deployment", () => {
+    const name = "Dapp University";
+    const symbol = "DAPP";
+    const decimals = "18";
+    const totalSupply = tokens(1000000);
+
     it("has correct name", async () => {
-      expect(await token.name()).to.equal("Dapp University");
+      expect(await token.name()).to.equal(name);
     });
 
     it("has correct symbol", async () => {
-      expect(await token.symbol()).to.equal("DAPP");
+      expect(await token.symbol()).to.equal(symbol);
     });
 
     it("has correct decimals", async () => {
-      expect(await token.decimals()).to.equal("18");
+      expect(await token.decimals()).to.equal(decimals);
     });
 
     it("has correct total supply", async () => {
-      expect(await token.totalSupply()).to.equal(tokens(1000000));
+      expect(await token.totalSupply()).to.equal(totalSupply);
     });
   });
 });
