995e311ad8e410a367f61d1b494004b9fd097f17
Ravi Tokas <ravitokas90@gmail.com>
2023-08-21 19:41:40 +0530

    add deployment describe block

--- test/Token.js
+++ test/Token.js
@@ -13,19 +13,21 @@ describe("Token", () => {
     token = await Token.deploy("Dapp University", "DAPP", 1000000);
   });
 
-  it("has correct name", async () => {
-    expect(await token.name()).to.equal("Dapp University");
-  });
+  describe("Deployment", () => {
+    it("has correct name", async () => {
+      expect(await token.name()).to.equal("Dapp University");
+    });
 
-  it("has correct symbol", async () => {
-    expect(await token.symbol()).to.equal("DAPP");
-  });
+    it("has correct symbol", async () => {
+      expect(await token.symbol()).to.equal("DAPP");
+    });
 
-  it("has correct decimals", async () => {
-    expect(await token.decimals()).to.equal("18");
-  });
+    it("has correct decimals", async () => {
+      expect(await token.decimals()).to.equal("18");
+    });
 
-  it("has correct total supply", async () => {
-    expect(await token.totalSupply()).to.equal(tokens(1000000));
+    it("has correct total supply", async () => {
+      expect(await token.totalSupply()).to.equal(tokens(1000000));
+    });
   });
 });
