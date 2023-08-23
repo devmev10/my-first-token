18adbac247e742a129baf62ea67b77d4948d3371
Ravi Tokas <ravitokas90@gmail.com>
2023-08-23 14:14:49 +0530

    update Token.sol

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -77,6 +77,8 @@ contract Token {
         address _to,
         uint256 _value
     ) public returns (bool success) {
+        require(_value <= allowance[_from][msg.sender]);
+
         _transfer(_from, _to, _value);
         return true;
     }
